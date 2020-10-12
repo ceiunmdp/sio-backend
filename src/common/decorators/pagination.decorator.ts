ae84cee898920eceff2ed7a5ff8bc5705a486ccd
Manuel Nucci <32201854+manuelnucci@users.noreply.github.com>
2020-10-12 19:36:32 -0300

    refactor: parametrize default and max pagination limit (#65)

--- src/common/decorators/pagination.decorator.ts
+++ src/common/decorators/pagination.decorator.ts
@@ -1,8 +1,9 @@
 import { DefaultValuePipe, ParseIntPipe, Query } from '@nestjs/common';
+import { DEFAULT_LIMIT } from '../constants/limits';
 import { PaginationLimitPipe } from '../pipes/pagination-limit.pipe';
 
 export const Limit = (property = 'limit') =>
-  Query(property, new DefaultValuePipe(10), ParseIntPipe, PaginationLimitPipe);
+  Query(property, new DefaultValuePipe(DEFAULT_LIMIT), ParseIntPipe, PaginationLimitPipe);
 
 export const Page = (property = 'page') => Query(property, new DefaultValuePipe(1), ParseIntPipe);
 
--- src/common/pipes/pagination-limit.pipe.ts
+++ src/common/pipes/pagination-limit.pipe.ts
@@ -1,8 +1,9 @@
 import { Injectable, PipeTransform } from '@nestjs/common';
+import { DEFAULT_LIMIT, MAX_LIMIT } from '../constants/limits';
 
 @Injectable()
 export class PaginationLimitPipe implements PipeTransform<number, number> {
   transform(limit: number) {
-    return limit <= 0 ? 10 : limit > 25 ? 25 : limit;
+    return limit <= 0 ? DEFAULT_LIMIT : limit > MAX_LIMIT ? MAX_LIMIT : limit;
   }
 }
